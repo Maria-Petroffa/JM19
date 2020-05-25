3d5e4846b2182196c8373e3fc95e45d48c8b80cd
Maria Petrova <Maria Petrova>
2020-05-25 19:05:40 +0300

    Updates

---
+++ precache-manifest.e9d9b094c3358cde62f16819230c565e.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e031bdccca952342269a70b8e4792a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bad6ab9640751765434",
+    "url": "/static/js/main.b4f5dacb.chunk.js"
+  },
+  {
+    "revision": "2819e3ce613f83569cad",
+    "url": "/static/js/runtime-main.dc0f253a.js"
+  }
+]);
---
+++ static/js/main.b4f5dacb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpaviasales=this.webpackJsonpaviasales||[]).push([[0],[function(s,a,i){s.exports=i(1)},function(s,a){}],[[0,1]]]);
+//# sourceMappingURL=main.b4f5dacb.chunk.js.map
